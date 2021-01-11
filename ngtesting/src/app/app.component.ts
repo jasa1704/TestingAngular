843b1a7c5d18dbb03dd3dbb8a2f5231774fccefd
Jhonatan Alveiro Sosapanta Arenas <sosa800026@siigo.com>
2021-01-10 19:27:13 -0500

    Testing SumApp toEqual

--- ngtesting/src/app/app.component.ts
+++ ngtesting/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  @ViewChild('result') result:any;
+  @ViewChild('num1') num1:any;
+  @ViewChild('num2') num2:any;
   title = 'ngtesting';
+
+  public ngOnInit(): void {
+
+  }
+
+  public add(num1: number, num2: number): number {
+    return Number(num1) + Number(num2);
+  }
+
+  public printAdd(): any {
+    const num1 = this.num1.nativeElement.value;
+    const num2 = this.num1.nativeElement.value;
+    this.result.nativeElement.value = this.add(num1,num2);
+  }
 }
